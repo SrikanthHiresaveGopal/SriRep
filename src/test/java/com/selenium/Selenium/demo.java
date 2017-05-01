5607ad3f60c8839326d71af220e3f04906573e7e
SrikanthHiresaveGopal <srikanth.mathru@gmail.com>
2017-05-01 01:48:52 -0400

    first commit

---
+++ src/test/java/com/selenium/Selenium/demo.java
@@ -0,0 +1,16 @@
+package com.selenium.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://facebook.com");
+
+	}
+
+}
